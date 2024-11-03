d4310045d609e9d0bbd35f18e23a8be719059f28
Eid LeWeise <61235522+eidleweise@users.noreply.github.com>
2024-11-03 22:21:19 +0000

    Initial upload
    Initial (and probably only) upload unless I figure out how to make it run automagically etc

---
+++ toggleDuplicateDisplay.ps1
@@ -0,0 +1,25 @@
+# displayswitch /internal Use one monitor, the internal one, only.
+# displayswitch /clone Show the same content on both (all?) monitors.
+# displayswitch /extend Make content larger, allow mouse to move from one monitor to the other.
+# displayswitch /external Use one monitor, the external one, only.
+
+
+# Check if the file exists
+if (Test-Path "toggleDisplayStatus.txt") {
+    $fileContent = Get-Content "toggleDisplayStatus.txt" | Select-Object -First 1
+
+    switch ($fileContent.Trim().ToLower()) {
+        "internal" {
+            displayswitch /extend
+            Set-Content "toggleDisplayStatus.txt" "extend"
+        }
+        default {
+            displayswitch /internal
+            Set-Content "toggleDisplayStatus.txt" "internal"
+        }
+    }
+} else {
+    # File does not exist, default to internal
+    displayswitch /internal
+    Set-Content "toggleDisplayStatus.txt" "internal"
+}
